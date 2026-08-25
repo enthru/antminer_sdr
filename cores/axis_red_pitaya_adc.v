87e5954fc4845df79ed7d24c8ab9af1da0685f57
enthru <enthru@enthru.net>
2026-08-25 22:32:49 +0300

    fix adc core

--- cores/axis_red_pitaya_adc.v
+++ cores/axis_red_pitaya_adc.v
@@ -1,22 +1,32 @@
-module axis_red_pitaya_adc # 
-( 
-	parameter integer DAC_DATA_WIDTH = 12 
-) 
-( 
-	input wire aclk, 
-	output wire adc_csn, 
-	input wire [11:0] adc_dat_a, 
-	input wire [11:0] adc_dat_b, 
-	output wire m_axis_tvalid, 
-	output wire [31:0] m_axis_tdata 
+`timescale 1 ns / 1 ps
+
+module axis_red_pitaya_adc #
+(
+  parameter integer DAC_DATA_WIDTH = 12
+)
+(
+  input  wire        aclk,
+  output wire        adc_csn,
+  input  wire [11:0] adc_dat_a,
+  input  wire [11:0] adc_dat_b,
+  output wire        m_axis_tvalid,
+  output wire [31:0] m_axis_tdata
 );
 
-assign adc_csn = 1'b1; 
-assign m_axis_tvalid = 1'b1;
+  reg [11:0] adc_dat_a_reg;
+  reg [11:0] adc_dat_b_reg;
 
-wire signed [15:0] data_a_signed = $signed({1'b0, adc_dat_a}) - 13'sd2048; 
-wire signed [15:0] data_b_signed = $signed({1'b0, adc_dat_b}) - 13'sd2048;
+  always @(posedge aclk)
+  begin
+    adc_dat_a_reg <= adc_dat_a;
+    adc_dat_b_reg <= adc_dat_b;
+  end
 
-assign m_axis_tdata = {data_b_signed, data_a_signed};
+  wire signed [15:0] data_a_signed = $signed({1'b0, adc_dat_a_reg}) - 13'sd2048;
+  wire signed [15:0] data_b_signed = $signed({1'b0, adc_dat_b_reg}) - 13'sd2048;
+
+  assign adc_csn       = 1'b1;
+  assign m_axis_tvalid = 1'b1;
+  assign m_axis_tdata  = {data_b_signed, data_a_signed};
 
 endmodule
